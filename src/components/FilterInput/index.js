e4c4d422423e1744f7fb9e3025c5f6e413871817
carmen-tm <carmentorremolina@gmail.com>
2019-06-19 14:57:48 +0200

    Hide labels but keeps a11y

--- src/components/FilterInput/index.js
+++ src/components/FilterInput/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './styles.scss';
+
 const FilterInput = props => {
 	const { type, placeholder, handlerInputName, nameValue } = props;
 	return (
 		<div>
-			<label htmlFor="name">Filter by name</label>
+			<label className="visually-hidden" htmlFor="name">
+				Filter by name
+			</label>
 			<input
 				type={type}
 				placeholder={placeholder}
