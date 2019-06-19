5134b8480589c5b426c52a5ebf61952806b57d28
carmen-tm <carmentorremolina@gmail.com>
2019-06-19 15:01:19 +0200

    Update production version

---
+++ docs/precache-manifest.9228b4c046e2d04c4e5a1dd5f3b6667a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241fc7e551d2d08f8b1bef5b70b90108",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0565f43796212fc9bbb5",
+    "url": "./static/css/main.6de56588.chunk.css"
+  },
+  {
+    "revision": "3b948c81417401d861dc",
+    "url": "./static/js/2.f1f8058c.chunk.js"
+  },
+  {
+    "revision": "0565f43796212fc9bbb5",
+    "url": "./static/js/main.e603743f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
